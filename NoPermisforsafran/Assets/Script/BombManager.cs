cc76ea89efd29ddf3754d2fa68c5401de9af9c09
P3D\judith.garnier <judithgarnier4@gmail.com>
2024-01-16 14:54:54 +0100

    player detection

--- NoPermisforsafran/Assets/Script/BombManager.cs
+++ NoPermisforsafran/Assets/Script/BombManager.cs
@@ -6,24 +6,22 @@ public class BombManager : MonoBehaviour
 {
 
     public GameObject BombPrefab;
-    public float ColliderSize;
-    [SerializeField] private LayerMask whatToHit;
+    public int RadiusSize;
 
+    [SerializeField] private LayerMask _whatToHit;
     private GameObject _lastestBomb;
     bool _isThereBomb = false;
     float _timerMax;
     float _timer = 0;
-    private BoxCollider _boxCollider;
+
 
     private void Awake()
     {
-        _boxCollider = GetComponent<BoxCollider>();
     }
 
     // Start is called before the first frame update
     public void Start()
     {
-        _boxCollider.size = ColliderSize * Vector3.one;
     }
 
     // Update is called once per frame
@@ -42,7 +40,7 @@ public class BombManager : MonoBehaviour
 
         else if (_timer <= 0 && _isThereBomb)
         {
-            Collider[] col = Physics.OverlapSphere(_lastestBomb.transform.position, 6, whatToHit);
+            Collider[] col = Physics.OverlapSphere(_lastestBomb.transform.position, RadiusSize, _whatToHit);
 
             foreach (var item in col)
             {
