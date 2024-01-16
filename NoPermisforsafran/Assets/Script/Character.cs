30e4f09c2c5acee4d591258accf33ce922178307
P3D\judith.garnier <judithgarnier4@gmail.com>
2024-01-16 11:11:14 +0100

    Chara modified

--- NoPermisforsafran/Assets/Script/Character.cs
+++ NoPermisforsafran/Assets/Script/Character.cs
@@ -7,7 +7,6 @@ public class Character : MonoBehaviour
 {
     public float playerSpeed;
     CharacterController controller;
-    public int Gold;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +17,6 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UI.instance._goldText.text = "Gold : " + Gold.ToString();
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
        controller.Move(move * Time.deltaTime * playerSpeed);
