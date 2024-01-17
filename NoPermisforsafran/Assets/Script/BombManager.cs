b3265b0f4a1179652ed1d649d2166aa739259db4
P3D\tom.lecorvic <tom.le-corvic@lacatholille.fr>
2024-01-17 11:17:15 +0100

    Integration FX Bomb

--- NoPermisforsafran/Assets/Script/BombManager.cs
+++ NoPermisforsafran/Assets/Script/BombManager.cs
@@ -7,11 +7,13 @@ public class BombManager : MonoBehaviour
     [Header("References")] [SerializeField]
     private GameObject _player1;
     [SerializeField] private GameObject _bombPrefab;
+    [SerializeField] private GameObject _areabombPrefab;
     [SerializeField] private LayerMask _targetLayer;
     public bool BombPermited;
 
     [Space(10)][Header("Bomb Properties Debug")]
     [SerializeField] private GameObject _lastestBomb;
+    [SerializeField] private GameObject _previewBomb;
     [SerializeField] private float _timerMax;
     [SerializeField] private float _currentTimer;
 
@@ -63,6 +65,7 @@ public class BombManager : MonoBehaviour
             }
 
             Destroy(_lastestBomb, 0.2f);
+            Destroy(_previewBomb, 0.2f);
 
             BombPermited = true;
             _currentTimer = _timerMax;
@@ -84,6 +87,7 @@ public class BombManager : MonoBehaviour
     private void CreateBomb()
     {
         _lastestBomb = Instantiate(_bombPrefab, _player1.transform.position, Quaternion.identity);
+        _previewBomb = Instantiate(_areabombPrefab, _player1.transform.position, Quaternion.identity);
         BombPermited = false;
         
         //Recursive bomb
