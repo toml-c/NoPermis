d25b20616576fbfe22973ed20a08d59c0e21ddb1
P3D\judith.garnier <judithgarnier4@gmail.com>
2024-01-17 12:10:25 +0100

    Victory Condition

--- NoPermisforsafran/Assets/Script/VictoryChecker.cs
+++ NoPermisforsafran/Assets/Script/VictoryChecker.cs
@@ -4,17 +4,40 @@ public class VictoryChecker : MonoBehaviour
 {
     public GameObject _feedBackInput;
 
-    [Space(10)][Header("Debug")]
+    [Space(10)]
+    [Header("Debug")]
     //[SerializeField] private bool _isStolen = false;
     [SerializeField] private int _playerCount = 0;
-    
+
     private bool _leftPressed = false;
     private bool _rightPressed = false;
 
     private void Update()
     {
         _feedBackInput.SetActive(_playerCount != 0);
+
+
+        if (_playerCount != 0)
+        {
+
+            if (_rightPressed == false && Input.GetKeyDown(KeyCode.RightControl))
+            {
+                _rightPressed = true;
+            }
+
+            if (_leftPressed == false && Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                _leftPressed = true;
+            }
+
+            if (_rightPressed == true && _leftPressed == true)
+            {
+                Debug.Log("You Won!");
+            }
+        }
+
     }
+
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -24,27 +47,14 @@ public class VictoryChecker : MonoBehaviour
         {
             BombManager.instance.BombPermited = false;
         }
-
-        if (_rightPressed == false && Input.GetKeyUp(KeyCode.RightControl))
-        {
-            _rightPressed = true;
-        }
-
-        if (_leftPressed == false && Input.GetKeyUp(KeyCode.LeftControl))
-        {
-            _leftPressed = true;
-        }
-
-        if (_rightPressed == true && _leftPressed == true)
-        {
-            Debug.Log("You Won!");
-        }
     }
+
+   
 
     private void OnTriggerExit(Collider collision)
     {
         _playerCount--;
-        
+
         if (_playerCount == 0 || collision.gameObject.name == "Player2")
         {
             BombManager.instance.BombPermited = true;
